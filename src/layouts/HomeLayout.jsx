8271a5cf462abfc0ced56f586411d34ba351b7f8
Your Name <you@example.com>
2025-08-17 14:23:25 +0600

    navbar

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from "react-router";
 import NavBar from "../components/NavBar";
-import Footer from "../components/NavBar";
+import Footer from "../components/Footer";
 const HomeLayout = () => {
     return (
         <div>
