9c9a32f35640e597fdf32806628cb6b6ed41fc7e
Your Name <you@example.com>
2025-08-24 18:34:28 +0600

    firebase deploy final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,52 +3,60 @@ import Banner from "../components/Banner";
 import QualitySection from "../components/QualitySection";
 import FollowUs from "../components/FollowUs";
 import AllCoffee from "../components/AllCoffee";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import QandA from "../components/QandA";
 import Reviews from "../components/Reviews";
 
 const Home = () => {
-   useEffect(() => {
-      document.title = "Espresso-Emporium";
-    }, []);
+  useEffect(() => {
+    document.title = "Espresso-Emporium";
+  }, []);
+
   return (
     <div>
       <Banner />
       <QualitySection />
 
-     <div className="bg-white"
-     style={{
-        backgroundImage: "url('/more/11.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}>
-      <section className="py-12 "
-      style={{
-        backgroundImage: "url('/more/13.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-      
+      <div
+        className="bg-white"
+        style={{
+          backgroundImage: "url('/more/11.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       >
-        <h2 className="text-4xl font-bold text-center text-[#331A15] mb-8">
-           Coffee Selection 
-        </h2>
-        <p className="text-center text-gray-600 max-w-2xl mx-auto mb-10">
-          Discover  finest selection of coffee. Here are some of  popular
-          blends crafted with love and passion from different business.
-        </p>
+        <section
+          className="py-12"
+          style={{
+            backgroundImage: "url('/more/13.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <h2 className="text-4xl font-bold text-center text-[#331A15] mb-8">
+            Coffee Selection
+          </h2>
+          <p className="text-center text-gray-600 max-w-2xl mx-auto mb-10">
+            Discover the finest selection of coffee. Here are some of the popular
+            blends crafted with love and passion from different businesses.
+          </p>
 
-        {/* Show only 6 coffees */}
-        <AllCoffee limit={2} />
-      </section>
-      
-      <QandA></QandA>
-      <Reviews></Reviews>
-      <FollowUs />
+          <AllCoffee limit={2} />
 
-     </div>
+          <div className="text-center mt-6">
+            <NavLink
+              to="/coffee-store"
+              className="inline-block bg-[#331A15] text-white px-6 py-2 rounded hover:bg-[#573322] transition"
+            >
+              Show More
+            </NavLink>
+          </div>
+        </section>
 
-      
+        <QandA />
+        <Reviews />
+        <FollowUs />
+      </div>
     </div>
   );
 };
